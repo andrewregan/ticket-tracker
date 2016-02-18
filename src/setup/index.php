9344a9dfabfa0181e07305bce6ac0b7c4cc31dff
Andrew Regan <a.jamesregan@gmail.com>
2016-02-18 17:45:13 -0500

    Add template for setup file

---
+++ src/setup/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Andrew Regan">
+
+    <title>Ticket Tracker Setup</title>
+
+    <!-- CSS dependencies -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- JavaScript dependencies -->
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
