7a86e783c8d675b731d303305f60c79f4b32457f
Andrew Regan <a.jamesregan@gmail.com>
2016-03-24 18:10:19 -0400

    Add blank modals to main page

--- public_html/index.php
+++ public_html/index.php
@@ -16,6 +16,44 @@
   <body>
     <?php include(dirname(__FILE__) . '/../resources/navbar.php'); ?>
 
+    <!-- Cancel ticket modal -->
+    <div class="modal fade" id="cancelTicketModal" role="dialog" aria-labelledby="cancelTicketModal" tabindex="-1">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button class="close" data-dismiss="modal" type="button" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="cancelTicketModal">Cancel Ticket</h4>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-default" data-dismiss="modal" type="button">Close</button>
+            <button class="btn btn-primary" type="button">Cancel Tickets</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Reserve ticket modal -->
+    <div class="modal fade" id="reserveTicketModal" role="dialog" aria-labelledby="reserveTicketModal" tabindex="-1">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button class="close" data-dismiss="modal" type="button" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="reserveTicketModal">Reserve Ticket</h4>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-default" data-dismiss="modal" type="button">Close</button>
+            <button class="btn btn-success" type="button">Reserve Tickets</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="container" id="content">
       <div class="panel panel-default no-border">
         <div class="panel-body no-padding">
@@ -30,22 +68,17 @@
         <div class="panel-footer" style="padding: 5px; border: none;">
           <div class="row">
             <div class="col-md-3" style="margin-bottom: 5px;">
-              <a class="btn btn-danger btn-lg btn-block" href="#cancelTickets">Cancel Tickets</a>
+              <a class="btn btn-danger btn-lg btn-block" data-target="#cancelTicketModal" data-toggle="modal" type="button">Cancel Ticket</a>
             </div>
             <div class="col-md-9">
-              <a class="btn btn-primary btn-lg btn-block" href="#orderTickets">Reserve Tickets</a>
+              <a class="btn btn-success btn-lg btn-block" data-target="#reserveTicketModal" data-toggle="modal" type="button">Reserve Ticket</a>
             </div>
           </div>
         </div>
       </div>
-
-      <a name="orderTickets"></a>
-      <div class="panel panel-primary large-margin">
-        <div class="panel-heading">Order Tickets:</div>
-        <div class="panel-body">
-          <div style="padding: 1000px;"></div>
-        </div>
-      </div>
     </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
